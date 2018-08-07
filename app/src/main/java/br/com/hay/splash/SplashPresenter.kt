820bc2155be6ab7f907e1d110cb3b24d3644da49
Carlos Gabriel <cgss@cesar.org.br>
2018-08-07 14:32:35 -0300

    Remove useless imports

--- app/src/main/java/br/com/hay/splash/SplashPresenter.kt
+++ app/src/main/java/br/com/hay/splash/SplashPresenter.kt
@@ -1,8 +1,5 @@
 package br.com.hay.splash
 
-import br.com.hay.app.HayApplication
-import br.com.hay.router.Router
-
 class SplashPresenter : SplashContract.Presenter {
 
     override fun start() {
