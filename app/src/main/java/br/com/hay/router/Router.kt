966b41b62900608ddec1c2fd70da76e39b0be9c0
Haldny Santos <haldnysantos10@gmail.com>
2018-08-16 16:10:03 -0300

    [HAY-02] - Create Login, Register and Forgot Password Activities
    Change-Id: I6f1042806fbf90395080f8392b49ba4b219af4c1

--- app/src/main/java/br/com/hay/router/Router.kt
+++ app/src/main/java/br/com/hay/router/Router.kt
@@ -1,8 +1,10 @@
 package br.com.hay.router
 
+import br.com.hay.forgotpassword.ForgotPasswordActivity
 import br.com.hay.wrapper.ContextWrapper
 
 import br.com.hay.login.LoginActivity
+import br.com.hay.register.RegisterActivity
 
 class Router : IRouter {
 
@@ -12,10 +14,15 @@ class Router : IRouter {
         mContext = context
     }
 
-
     override fun goNext(from : Int, event : String) {
         if (from.equals(IRouter.SPLASH_SCREEN) && event.equals(IRouter.SPLASH_SCREEN_FINISH)){
-            mContext.goNext(LoginActivity::class.java, true)
+            mContext?.goNext(LoginActivity::class.java, true)
+        } else if(from.equals(IRouter.LOGIN_SCREEN)
+                && event.equals(IRouter.LOGIN_SCREEN_REGISTER)) {
+            mContext?.goNext(RegisterActivity::class.java, false)
+        } else if (from.equals(IRouter.LOGIN_SCREEN)
+                && event.equals(IRouter.LOGIN_SCREEN_FORGOT_PASSWORD)) {
+            mContext?.goNext(ForgotPasswordActivity::class.java, false)
         }
     }
 }
--- app/src/main/java/br/com/hay/splash/SplashPresenter.kt
+++ app/src/main/java/br/com/hay/splash/SplashPresenter.kt
@@ -3,17 +3,21 @@ package br.com.hay.splash
 import br.com.hay.handler.IDelayHandler
 import br.com.hay.router.IRouter
 
-class SplashPresenter(val router : IRouter?, val delayHandler: IDelayHandler?) :
+private const val SPLASH_DELAY: Long = 3000
+
+class SplashPresenter(private val router: IRouter?, private val delayHandler: IDelayHandler?) :
         SplashContract.Presenter {
 
-    private val SPLASH_DELAY: Long = 3000
+    private var mView: SplashContract.View? = null
 
-    override fun start() {
+    override fun start(view: SplashContract.View) {
+        mView = view
         delayHandler?.start(SPLASH_DELAY, Runnable {
-            router?.goNext(IRouter.SPLASH_SCREEN, IRouter.SPLASH_SCREEN_FINISH) })
+            router?.goNext(IRouter.SPLASH_SCREEN, IRouter.SPLASH_SCREEN_FINISH)})
     }
 
     override fun finish(){
+        mView = null
         delayHandler?.destroy()
     }
 }
